41affd670b8e771ce0b6989f5e5a2c4999e2a405
indigane <indigane@users.noreply.github.com>
2023-06-22 15:33:01 +0300

    Change agelimit page success criteria

--- js/passport-page-agelimit.js
+++ js/passport-page-agelimit.js
@@ -97,8 +97,8 @@ allowedGridsForIcons['icon-5'] = [initialGrid, ...person3Grids];
 allowedGridsForIcons['icon-6'] = [initialGrid, ...person3Grids];
 allowedGridsForIcons['icon-7'] = [initialGrid, ...person2Grids, ...person3Grids];
 allowedGridsForIcons['icon-8'] = [initialGrid, ...person3Grids];
-allowedGridsForIcons['icon-9'] = [initialGrid, ...person2Grids, ...person3Grids];
-allowedGridsForIcons['icon-10'] = [initialGrid, ...person1Grids, ...person2Grids, ...person3Grids];
+allowedGridsForIcons['icon-9'] = [initialGrid, ...person3Grids];
+allowedGridsForIcons['icon-10'] = [initialGrid, ...person2Grids, ...person3Grids];
 
 function getAllowedGridsForItem(item) {
   const iconId = item.getElement().getAttribute('data-id');
@@ -162,8 +162,38 @@ function handleDragEnd() {
 //
 allGrids.forEach(grid => grid.on('dragReleaseEnd', checkSuccess));
 function checkSuccess() {
-  const initialGridRemainingItemCount = document.querySelectorAll('.agelimit-initial-grid .agelimit-item').length;
-  if (initialGridRemainingItemCount === 0) {
+  function isSuccess() {
+    const minimumItemCountPerPerson = 2;
+    let person1ItemCount = 0;
+    let person2ItemCount = 0;
+    let person3ItemCount = 0;
+    for (const targetGrid of person1Grids) {
+      person1ItemCount += targetGrid.getItems().length;
+    }
+    if (person1ItemCount < minimumItemCountPerPerson) {
+      return false;
+    }
+    for (const targetGrid of person2Grids) {
+      person2ItemCount += targetGrid.getItems().length;
+    }
+    if (person2ItemCount < minimumItemCountPerPerson) {
+      return false;
+    }
+    for (const targetGrid of person3Grids) {
+      person3ItemCount += targetGrid.getItems().length;
+    }
+    if (person3ItemCount < minimumItemCountPerPerson) {
+      return false;
+    }
+    const requiredIcons = '[data-id="icon-1"], [data-id="icon-2"], [data-id="icon-3"]';
+    const ageSymbolsInInitialGridCount = document.querySelectorAll(`.agelimit-initial-grid .agelimit-item:is(${requiredIcons})`).length;
+    // All age symbols must be dragged
+    if (ageSymbolsInInitialGridCount === 0) {
+      return true;
+    }
+    return false;
+  }
+  if (isSuccess()) {
     continueButton.classList.add('active');
     successSound.play();
   }
